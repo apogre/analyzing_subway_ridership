35569d203608bd78b0f5ffc21f8cde233bb2ec2b
anishpradhan <mail.anishpradhan@gmail.com>
2018-04-06 18:25:22 -0700

    functions

--- nyc_turnstile.py
+++ nyc_turnstile.py
@@ -3,12 +3,6 @@ import pandas
 from ggplot import *
 import sys
 
-"""
-In this question, you need to:
-1) implement the compute_cost() and gradient_descent() procedures
-2) Select features (in the predictions procedure) and make predictions.
-
-"""
 
 def normalize_features(df):
     """
@@ -29,28 +23,17 @@ def compute_cost(features, values, theta):
     """
     Compute the cost function given a set of features / values, 
     and the values for our thetas.
-    
-    This can be the same code as the compute_cost function in the lesson #3 exercises,
-    but feel free to implement your own.
     """
     
-    # your code here
     predicted = np.dot(features,theta)
     sum_of_sq = np.square(predicted - values).sum()
-    # print sum_of_sq
-    # print 1.0/(2.0*len(values))
     cost =  1.0/(2.0*len(values)) * (sum_of_sq)
-    # print cost
     return cost
 
 def gradient_descent(features, values, theta, alpha, num_iterations):
     """
     Perform gradient descent given a data set with an arbitrary number of features.
-    
-    This can be the same gradient descent code as in the lesson #3 exercises,
-    but feel free to implement your own.
     """
-    
     m = len(values)
     cost_history = []
 
@@ -60,42 +43,16 @@ def gradient_descent(features, values, theta, alpha, num_iterations):
         theta = theta + (alpha/m)*pred
         cost = compute_cost(features, values, theta)
         cost_history.append(cost)
-        # pass
-        # your code here
     return theta, pandas.Series(cost_history)
 
 def predictions(dataframe):
     '''
-    The NYC turnstile data is stored in a pandas dataframe called weather_turnstile.
-    Using the information stored in the dataframe, let's predict the ridership of
-    the NYC subway using linear regression with gradient descent.
+    let's predict the ridership of the NYC subway using linear regression with gradient descent.
     
     You can download the complete turnstile weather dataframe here:
-    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv    
-    
-    Your prediction should have a R^2 value of 0.40 or better.
-    You need to experiment using various input features contained in the dataframe. 
-    We recommend that you don't use the EXITSn_hourly feature as an input to the 
-    linear model because we cannot use it as a predictor: we cannot use exits 
-    counts as a way to predict entry counts. 
-    
-    Note: Due to the memory and CPU limitation of our Amazon EC2 instance, we will
-    give you a random subet (~15%) of the data contained in 
-    turnstile_data_master_with_weather.csv. You are encouraged to experiment with 
-    this computer on your own computer, locally. 
-    
-    
-    If you'd like to view a plot of your cost history, uncomment the call to 
-    plot_cost_history below. The slowdown from plotting is significant, so if you 
-    are timing out, the first thing to do is to comment out the plot command again.
-    
-    If you receive a "server has encountered an error" message, that means you are 
-    hitting the 30-second limit that's placed on running your program. Try using a 
-    smaller number for num_iterations if that's the case.
-    
-    If you are using your own algorithm/models, see if you can optimize your code so 
-    that it runs faster.
-    '''
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv     
+      
+   '''
     # Select Features (try different features!)
     features = dataframe[['rain', 'precipi', 'hour', 'meantempi']]
     
@@ -133,12 +90,6 @@ def predictions(dataframe):
     # Uncomment the next line to see your cost history
     # -------------------------------------------------
     plot = plot_cost_history(alpha, cost_history)
-    # 
-    # Please note, there is a possibility that plotting
-    # this in addition to your calculation will exceed 
-    # the 30 second limit on the compute servers.
-
-
     
     predictions = np.dot(features_array, theta_gradient_descent)
     r_sq = r_squared(values,predictions)
@@ -147,14 +98,6 @@ def predictions(dataframe):
 
 def plot_cost_history(alpha, cost_history):
    """This function is for viewing the plot of your cost history.
-   You can run it by uncommenting this
-
-       plot_cost_history(alpha, cost_history) 
-
-   call in predictions.
-   
-   If you want to run this locally, you should print the return value
-   from this function.
    """
    cost_df = pandas.DataFrame({
       'Cost_History': cost_history,
@@ -165,20 +108,22 @@ def plot_cost_history(alpha, cost_history):
 
 
 def r_squared(values, predicted):
+    #compute r_squared value
+
     avg_val = np.mean(values)
     num = np.square(predicted-avg_val).sum()
     den = np.square(values-avg_val).sum()
     r_sq = num/den
     return r_sq
 
-file = "data/turnstile_weather_v2.csv"
-weather_turnstile = pandas.read_csv(file)
-print list(weather_turnstile)
+if __name__ == '__main__':
+    
+    input_file = "data/turnstile_weather_v2.csv"
+    weather_turnstile = pandas.read_csv(file)
 
-
-predictions, plot, r_sq = predictions(weather_turnstile)
-
-print r_sq
-
-print predictions
-print plot
+    # list of features
+    print list(weather_turnstile)
+    predictions, plot, r_sq = predictions(weather_turnstile)
+    print r_sq
+    print predictions
+    print plot
