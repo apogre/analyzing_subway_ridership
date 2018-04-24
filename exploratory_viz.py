d1d214c35a556d04aace693cf2df3327a2314801
apogre <mail.anishpradhan@gmail.com>
2018-04-23 22:30:45 -0700

    plots

--- exploratory_viz.py
+++ exploratory_viz.py
@@ -4,10 +4,12 @@ from ggplot import *
 import sys
 
 # Ridership by time of day or day of week
-def ridership_time(weather_turnstile):
+def ridership_time(rider_df):
 	#use ENTRIESn_hourly,hour,day_week
-	pass
-
+	plot = ggplot(rider_df, aes(x=reorder('factor(hour)'),y='ENTRIESn_hourly')) + geom_point()+geom_line()
+	print plot
+	
+	
 if __name__ == '__main__':
     
     input_file = "data/turnstile_weather_v2.csv"
@@ -15,4 +17,9 @@ if __name__ == '__main__':
 
     # list of features
     print list(weather_turnstile)
-    print weather_turnstile.head(2)
+    # print weather_turnstile.head(2)
+    rider_time = weather_turnstile.groupby(by=['hour'])['ENTRIESn_hourly'].sum()
+    rider_df = rider_time.to_frame().reset_index()
+    print rider_df
+    print list(rider_df)
+    ridership_time(rider_df)
